768723a8a13ba613ecbd6b8327825bf9d7c7b830
dode2511 <81193494+dode2511@users.noreply.github.com>
2023-12-06 09:55:32 -0300

    a

--- databases/conecta.js
+++ databases/conecta.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
+//import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
 
 // export const sequelize = new Sequelize(
 //   DB_NAME, DB_USER, DB_PASSWORD, {
@@ -13,7 +13,7 @@ import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
 
 
 export const sequelize = new Sequelize(
-  "teste", "root", "dode2511", {
+  "teste", "aluno", "senacrs", {
   dialect: "mysql",
   host: "localhost",
   port: 3306
