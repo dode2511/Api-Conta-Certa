fa5c0b920a5cd914ac01bcbd93036f17eb3fdd28
Pedro Menna <113869595+pedromenna@users.noreply.github.com>
2023-12-06 09:27:26 -0300

    Update conecta.js

--- databases/conecta.js
+++ databases/conecta.js
@@ -1,20 +1,20 @@
 import { Sequelize } from 'sequelize';
 import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
 
-// export const sequelize = new Sequelize(
-//   DB_NAME, DB_USER, DB_PASSWORD, {
-//   dialect: "mysql",
-//   host: DB_HOST,
-//   port: DB_PORT
-// });
-
-
-
-
-
 export const sequelize = new Sequelize(
-  "teste", "root", "dode2511", {
+  DB_NAME, DB_USER, DB_PASSWORD, {
   dialect: "mysql",
-  host: "localhost",
-  port: 3306
+  host: DB_HOST,
+  port: DB_PORT
 });
+
+
+
+
+
+// export const sequelize = new Sequelize(
+//   "teste", "root", "dode2511", {
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306
+// });
