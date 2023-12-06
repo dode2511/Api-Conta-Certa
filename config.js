f850d67819bff9e3890f5c0c0bebe6d4b3f6610f
dode2511 <81193494+dode2511@users.noreply.github.com>
2023-12-06 10:18:07 -0300

    a

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 54168
 
 
-export const DB_HOST = process.env.DB_HOST|| `localhost`
+export const DB_HOST = process.env.DB_HOST|| `monorail.proxy.rlwy.net`
 export const DB_USER = process.env.DB_USER|| `root`
-export const DB_PASSWORD = process.env.DB_PASSWORD|| `dode2511`
-export const DB_NAME = process.env.DB_NAME|| `usersdb`
-export const DB_PORT = process.env.DB_PORT|| `3306`
+export const DB_PASSWORD = process.env.DB_PASSWORD|| `5b-Df5EAdacefEF5AGgA6bff2GAhhfAC`
+export const DB_NAME = process.env.DB_NAME|| `railway`
+export const DB_PORT = process.env.DB_PORT|| `54168`
 
 
