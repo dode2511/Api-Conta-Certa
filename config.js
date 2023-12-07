5a837892a9e4fbdd462d1f436c3c0ddb46393f41
dode2511 <leondodejr@gmail.com>
2023-12-07 15:56:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const PORT = process.env.PORT || 54168
 
 
-export const DB_HOST = process.env.DB_HOST|| `monorail.proxy.rlwy.net`
+export const DB_HOST = process.env.DB_HOST|| `containers-us-west-87.railway.app`
 export const DB_USER = process.env.DB_USER|| `root`
-export const DB_PASSWORD = process.env.DB_PASSWORD|| `5b-Df5EAdacefEF5AGgA6bff2GAhhfAC`
+export const DB_PASSWORD = process.env.DB_PASSWORD|| `8w20BUTThsgzC6X8ZxL5`
 export const DB_NAME = process.env.DB_NAME|| `railway`
-export const DB_PORT = process.env.DB_PORT|| `54168`
+export const DB_PORT = process.env.DB_PORT|| `6333`
 
 
