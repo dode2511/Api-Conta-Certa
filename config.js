beaa5a577a58c16b0c61ed49fa190b72d15b0e6e
dode2511 <leondodejr@gmail.com>
2023-12-07 16:05:09 -0300

    EDIT

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 export const PORT = process.env.PORT || 54168
 
 
-export const DB_HOST = process.env.DB_HOST|| `containers-us-west-87.railway.app`
-export const DB_USER = process.env.DB_USER|| `root`
-export const DB_PASSWORD = process.env.DB_PASSWORD|| `8w20BUTThsgzC6X8ZxL5`
-export const DB_NAME = process.env.DB_NAME|| `railway`
-export const DB_PORT = process.env.DB_PORT|| `6333`
+//export const DB_HOST = process.env.DB_HOST|| `containers-us-west-87.railway.app`
+//export const DB_USER = process.env.DB_USER|| `root`
+//export const DB_PASSWORD = process.env.DB_PASSWORD|| `8w20BUTThsgzC6X8ZxL5`
+//export const DB_NAME = process.env.DB_NAME|| `railway`
+//export const DB_PORT = process.env.DB_PORT|| `6333`
 
+
+
+export const MYSQLHOST = process.env.MYSQLHOST|| `containers-us-west-87.railway.app`
+export const MYSQLUSER = process.env.MYSQLUSER|| `root`
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD|| `8w20BUTThsgzC6X8ZxL5`
+export const MYSQL_DATABASE = process.env.MYSQL_DATABASE|| `railway`
+export const MYSQLPORT = process.env.MYSQLPORT|| `6333`
 
--- databases/conecta.js
+++ databases/conecta.js
@@ -1,11 +1,15 @@
 import { Sequelize } from 'sequelize';
-import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
+//import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
+
+
+import { MYSQLHOST,MYSQLPASSWORD,MYSQLUSER,MYSQLPORT,MYSQL_DATABASE } from '../config.js';
+
 
 export const sequelize = new Sequelize(
-  DB_NAME, DB_USER, DB_PASSWORD, {
+  MYSQL_DATABASE, MYSQLUSER, MYSQLPASSWORD, {
   dialect: "mysql",
-  host: DB_HOST,
-  port: DB_PORT
+  host: MYSQLHOST,
+  port: MYSQLPORT
 });
 
 
