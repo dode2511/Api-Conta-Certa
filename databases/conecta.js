83c0c86e6ad640a108f34135411f61263a00be8f
dode2511 <leondodejr@gmail.com>
2023-12-07 16:11:49 -0300

    a

--- databases/conecta.js
+++ databases/conecta.js
@@ -2,23 +2,23 @@ import { Sequelize } from 'sequelize';
 //import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from '../config.js';
 
 
-import { MYSQLHOST,MYSQLPASSWORD,MYSQLUSER,MYSQLPORT,MYSQL_DATABASE } from '../config.js';
+//import { MYSQLHOST,MYSQLPASSWORD,MYSQLUSER,MYSQLPORT,MYSQL_DATABASE } from '../config.js';
 
 
-export const sequelize = new Sequelize(
-  MYSQL_DATABASE, MYSQLUSER, MYSQLPASSWORD, {
+//export const sequelize = new Sequelize(
+//  MYSQL_DATABASE, MYSQLUSER, MYSQLPASSWORD, {
+//  dialect: "mysql",
+//  host: MYSQLHOST,
+//  port: MYSQLPORT
+//});
+
+
+
+
+
+ export const sequelize = new Sequelize(
+   "railway", "root", "eFC36HG-bfB4hgfFaCfhA1bbDhh3GAaG", {
   dialect: "mysql",
-  host: MYSQLHOST,
-  port: MYSQLPORT
+   host: "monorail.proxy.rlwy.net",
+   port: 52366
 });
-
-
-
-
-
-// export const sequelize = new Sequelize(
-//   "teste", "root", "dode2511", {
-//   dialect: "mysql",
-//   host: "localhost",
-//   port: 3306
-// });
