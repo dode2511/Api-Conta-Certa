e778daed821897da7ec9c94950b0fe1a4e5b25db
dode2511 <leondodejr@gmail.com>
2023-12-17 13:56:23 -0300

    Update conecta.js

--- databases/conecta.js
+++ databases/conecta.js
@@ -18,7 +18,7 @@ import { Sequelize } from 'sequelize';
 
  export const sequelize = new Sequelize(
  "railway", "root", "eFC36HG-bfB4hgfFaCfhA1bbDhh3GAaG", {
- dialect: "mysql",
+  dialect: "mysql",
   host: "monorail.proxy.rlwy.net",
   port: 52366
 });
